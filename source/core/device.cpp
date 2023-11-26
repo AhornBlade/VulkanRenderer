fe45d0cdf1f381b78e32b737b860c78529c0723a
AhornBlade <2417425957@qq.com>
2023-11-26 19:23:03 +0800

    add device

---
+++ source/core/device.cpp
@@ -0,0 +1,33 @@
+#include "device.hpp"
+
+namespace vkr
+{
+	DeviceCreateInfo::DeviceCreateInfo()
+	{
+#ifdef VULKAN_RENDERER_DEBUG
+		enabledLayers.push_back("VK_LAYER_KHRONOS_validation");
+#endif
+	}
+
+	Device::Device(const vk::raii::PhysicalDevice& physicalDevice, const DeviceCreateInfo& createInfo, const QueueFamilyInfos& queueFamilyInfos)
+		:vk::raii::Device{ getDevice(physicalDevice, createInfo, queueFamilyInfos) }{}
+
+	Device::Device(const vk::raii::PhysicalDevice& physicalDevice, const DeviceCreateInfo& createInfo)
+		:Device{ physicalDevice, createInfo, QueueFamilyInfos{physicalDevice} } {}
+
+	Device::Device(const vk::raii::PhysicalDevice& physicalDevice, const DeviceCreateInfo& createInfo, std::span<QueueRequirement> queueRequirements)
+		:Device{ physicalDevice, createInfo, QueueFamilyInfos{physicalDevice, queueRequirements} } {}
+
+	vk::raii::Device Device::getDevice(const vk::raii::PhysicalDevice& physicalDevice, const DeviceCreateInfo& deviceCreateInfo, const QueueFamilyInfos& queueFamilyInfos) const
+	{
+		std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos = queueFamilyInfos;
+
+		vk::DeviceCreateInfo createInfo;
+		createInfo.setPEnabledLayerNames(deviceCreateInfo.enabledLayers);
+		createInfo.setPEnabledExtensionNames(deviceCreateInfo.enabledExtensions);
+		createInfo.setPEnabledFeatures(&deviceCreateInfo.enabledFeatures);
+		createInfo.setQueueCreateInfos(queueCreateInfos);
+		
+		return vk::raii::Device{ physicalDevice, createInfo };
+	}
+}// namespace vkr
---
+++ source/core/device.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "queue.hpp"
+
+namespace vkr
+{
+	struct DeviceCreateInfo
+	{
+		DeviceCreateInfo();
+
+		std::vector<const char*> enabledLayers;
+		std::vector<const char*> enabledExtensions;
+		vk::PhysicalDeviceFeatures enabledFeatures;
+	};
+
+	class Device : public vk::raii::Device
+	{
+	public:
+		Device(const vk::raii::PhysicalDevice& physicalDevice,
+			const DeviceCreateInfo& createInfo,
+			const QueueFamilyInfos& queueFamilyInfos);
+
+		//GPU only mode
+		Device(const vk::raii::PhysicalDevice& physicalDevice,
+			const DeviceCreateInfo& createInfo);
+
+		Device(const vk::raii::PhysicalDevice& physicalDevice,
+			const DeviceCreateInfo& createInfo,
+			std::span<QueueRequirement> queueRequirements);
+
+	private:
+		vk::raii::Device getDevice(const vk::raii::PhysicalDevice& physicalDevice,
+			const DeviceCreateInfo& createInfo,
+			const QueueFamilyInfos& queueFamilyInfos) const;
+	};
+
+	template<class ... Ts>
+	Device createDevice(const vk::raii::PhysicalDevice& physicalDevice, std::span<QueueRequirement> queueRequirements)
+	{
+		DeviceCreateInfo createInfo{};
+		(Ts::setDeviceCreateInfo(createInfo), ...);
+		return Device{ physicalDevice, createInfo, queueRequirements };
+	}
+
+	//GPU only mode
+	template<class ... Ts>
+	Device createDevice(const vk::raii::PhysicalDevice& physicalDevice)
+	{
+		DeviceCreateInfo createInfo{};
+		(Ts::setDeviceCreateInfo(createInfo), ...);
+		return Device{ physicalDevice, createInfo };
+	}
+
+}// namespace vkr
